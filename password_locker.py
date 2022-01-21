fc4603bd703550946f9036f4e541af97c9946d4a
Alexander <alexndegwa46@gmail.com>
2022-01-21 17:04:15 +0300

    installed pyperclip

---
+++ password_locker.py
@@ -0,0 +1,112 @@
+import random
+import pyperclip
+import string
+
+#Global Variable
+global users_list
+class User:
+    '''
+    Class to create user accounts and save their information
+    '''
+    #class variables
+    #global users_list
+    users_list = []
+    def _init_(self, first_name, last_name, password):
+        '''
+        Methods to define the properties for each user object will hold
+        '''
+
+        #instance variables
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = password
+
+    def save_user(self):
+        '''
+        Function to save a newly created user instance
+        '''
+        User.users_list.append(self)
+
+class Credential:
+    '''
+    Class to create  account credentials, generate passwords and save their information
+    '''
+    #class variables
+    credentials_list = []
+    user_credentials_list = []
+    @classmethod
+    def check_user(cls, first_name, password):
+        '''
+        Method that checks if the name and password entered match entries in the users_list
+        '''
+        current_user = ''
+        for user in User.users_list:
+            if (user.first_name == first_name and user.password == password):
+                current_user = user.first_name
+            return current_user
+
+    def _init_(self, user_name, site_name, account_name, password):
+        '''
+        Method to define the properties for each user object will hold.
+        '''
+
+        #instance variables
+        self.user_name = user_name
+        self.site_name = site_name 
+        self.account_name = account_name
+        self.password = password
+
+    def save_credentials(self):
+        '''
+        Function to save a newly created user instance
+        '''
+        #global users_list
+        Credential.credentials_list.append(self)
+
+    def generate_password(size=8, char=string.ascii_lowercase+string.ascii_lowercase+string.digits):
+        '''
+        Function to generate an 8 character password for a credential
+        '''
+        gen_pass=''.join(random.choice(char) for _ in range(size)) 
+        return gen_pass
+
+    @classmethod
+    def display_credentials(cls, user_name):
+        '''
+        Class method to display the list of credentials saved
+        '''  
+        user_credentials_list = []
+        for credential in cls.credentials_list:
+            if credential.user_name == user_name:
+                user_credentials_list.append(credential)
+        return user_credentials_list
+
+
+    @classmethod
+    def display_credentials(cls, user_name):
+        '''
+        Class method to display the list of credentials saved
+        '''
+        user_credentials_list = []
+        for credential in cls.credentials_list:
+            if credential.user_name == user_name:
+                user_credentials_list.append(credential)
+        return user_credentials_list
+
+
+    @classmethod
+    def find_by_name(cls, site_name):
+        '''
+        Method that takes in a site_name and returns a credential that matches that of that site_name
+        '''
+        for credential in cls.credentials_list:
+            if credential.site_name == site_name:
+                return credential          
+
+    @classmethod
+    def copy_credentials(cls, site_name):
+        '''
+        Class method that copies a credentials info after the credentials site name is entered
+        '''
+        find_credential = Credential.find_by_site_name(site_name)
+        return pyperclip.copy(find_credential)            
